7bf8a4e23e9c84ad96bdd6eb622764daad5f4bb8
Paris Klein <70615009+parisvictorious@users.noreply.github.com>
2021-02-04 21:49:24 -0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1> Header and Logo </h1>
+       
+       {/* Title bar*/}
+       {/* Graph */}
+       {/* Tool Bar */}
+       {/* Data Input */}
+
+    
+    </div>
+  );
+}
+
+export default App;
